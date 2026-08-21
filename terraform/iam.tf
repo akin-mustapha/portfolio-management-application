ddf7cf18bf6a7f44bd9c48451811e2d88bfe18d8
Akinkunmi Mustapha <akinkunmimustapha1@example.com>
2026-08-21 02:25:48 +0100

    Add S3 delete permission to financial dataflow IAM policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -41,6 +41,7 @@ resource "aws_iam_role_policy" "financial_dataflow" {
         Action = [
           "s3:GetObject",
           "s3:PutObject",
+          "s3:DeleteObject",
           "s3:ListBucket"
         ]
         Resource = [
